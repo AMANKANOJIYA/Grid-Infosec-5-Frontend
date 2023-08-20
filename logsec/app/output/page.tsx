9620a072504cd06eceb836071a007695f2403f82
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2023-08-20 17:46:37 +0530

    Modal added

--- logsec/app/output/page.tsx
+++ logsec/app/output/page.tsx
@@ -1,103 +1,73 @@
 "use client";
 
-function model() {
+import React from "react";
+import { useState } from "react";
+
+interface data {
+  id: number;
+  logline: string;
+  problem: string;
+  probsol: string;
+}
+
+function Model({ showModal, setShowModal, activeData }: any) {
+  if (!showModal) {
+    return null;
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="model">
-      <div className="modelcontent">
-        <div className="modelheader">
-          <h1>Logline</h1>
-          <h3>Problem</h3>
-        </div>
-        <div className="modelbody">
-          <h3>Problem Solution</h3>
-        </div>
+    <div className="modal">
+      <div className="modalproblem">Problem: {activeData.problem}</div>
+      <div className="modallogline">{activeData.logline}</div>
+      <div className="modalprobsol">
+        <b>Solution:</b> {activeData.probsol}
+      </div>
+      <div className="modalclose" onClick={closeModal}>
+        Close
       </div>
     </div>
   );
 }
 
 export default function Output() {
+  const [showModal, setShowModal] = useState(true);
+  const [activeData, setActiveData] = useState<data>(data[0]);
   const openModel = (id: any) => {
-    console.log(id);
+    setShowModal(true);
+    setActiveData(data.filter((item: any) => item.id === id)[0]);
   };
-  const data = [
-    {
-      id: 1,
-      logline:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, sint.",
-      problem:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, iste! Lorem,sfsd fdsds fdfs fsfsfs dfsdfsdf sdfsdf",
-      probsol:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum neque maxime illo soluta, impedit voluptatibus rem vero quia sunt repellat repellendus optio. Veritatis ratione ab numquam alias sapiente eum ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil magnam fugiat cum iure. Repellendus quaerat, delectus corporis autem est dolore",
-    },
-    {
-      id: 2,
-      logline:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, sint.",
-      problem:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, iste! Lorem,sfsd fdsds fdfs fsfsfs dfsdfsdf sdfsdf",
-      probsol:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum neque maxime illo soluta, impedit voluptatibus rem vero quia sunt repellat repellendus optio. Veritatis ratione ab numquam alias sapiente eum ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil magnam fugiat cum iure. Repellendus quaerat, delectus corporis autem est dolore",
-    },
-    {
-      id: 3,
-      logline:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, sint.",
-      problem:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, iste! Lorem,sfsd fdsds fdfs fsfsfs dfsdfsdf sdfsdf",
-      probsol:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum neque maxime illo soluta, impedit voluptatibus rem vero quia sunt repellat repellendus optio. Veritatis ratione ab numquam alias sapiente eum ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil magnam fugiat cum iure. Repellendus quaerat, delectus corporis autem est dolore",
-    },
-    {
-      id: 4,
-      logline:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, sint.",
-      problem:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, iste! Lorem,sfsd fdsds fdfs fsfsfs dfsdfsdf sdfsdf",
-      probsol:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum neque maxime illo soluta, impedit voluptatibus rem vero quia sunt repellat repellendus optio. Veritatis ratione ab numquam alias sapiente eum ea. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil magnam fugiat cum iure. Repellendus quaerat, delectus corporis autem est dolore",
-    },
-  ];
   return (
     <>
-      <div className="navbar">
-        <img src="/flipkart.png" alt="Logo" className="logoimage" />
+      <div className={showModal ? "outputpage" : ""}>
+        <div className="navbar">
+          <img src="/flipkart.png" alt="Logo" className="logoimage" />
+        </div>
+        <div className="sectionoutput">
+          {data.map((item: any) => {
+            return (
+              <div
+                className="card"
+                key={item.id}
+                onClick={() => openModel(item.id)}
+              >
+                <div className="logline">{item.logline}</div>
+                <div className="problem">{item.problem}</div>
+                <div className="probsol">{item.probsol}</div>
+                <div className="showmore">Show more ...</div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div className="sectionoutput">
-        {data.map((item: any) => {
-          return (
-            <div
-              className="card"
-              key={item.id}
-              onClick={() => openModel(item.id)}
-            >
-              <div className="logline">{item.logline}</div>
-              <div className="problem">{item.problem}</div>
-              <div className="probsol">{item.probsol}</div>
-              <div className="showmore">Show more ...</div>
-            </div>
-          );
-        })}
-        {/* <div className="card">
-          <div className="logline">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod,
-            sint.
-          </div>
-          <div className="problem">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque,
-            iste! Lorem,sfsd fdsds fdfs fsfsfs dfsdfsdf sdfsdf
-          </div>
-          <div className="probsol">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum
-            neque maxime illo soluta, impedit voluptatibus rem vero quia sunt
-            repellat repellendus optio. Veritatis ratione ab numquam alias
-            sapiente eum ea. Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Nihil magnam fugiat cum iure. Repellendus quaerat, delectus
-            corporis autem est dolore
-          </div>
-          <div className="showmore">Show more ...</div>
-        </div> */}
-      </div>
+      <Model
+        showModal={showModal}
+        setShowModal={setShowModal}
+        activeData={activeData}
+      />
     </>
   );
 }
